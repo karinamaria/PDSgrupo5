4e398c74c4f6f7acf72129efcefe5f1e68064e93
mariaeloi <mariaeduardaeloi@outlook.com>
2021-06-30 00:28:43 -0300

    #2 - ajustes

--- src/main/java/br/ufrn/PDSgrupo5/controller/ProfissionalSaudeController.java
+++ src/main/java/br/ufrn/PDSgrupo5/controller/ProfissionalSaudeController.java
@@ -1,30 +1,49 @@
 package br.ufrn.PDSgrupo5.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import br.ufrn.PDSgrupo5.model.ProfissionalSaude;
 import br.ufrn.PDSgrupo5.service.ProfissionalSaudeService;
 
 @Controller
 @RequestMapping("profissional-saude")
 public class ProfissionalSaudeController {
-	
-	@Autowired
 	private ProfissionalSaudeService profissionalSaudeService;
 	
-	//Processamento formulário cadastro profissional de sáude
-	@PostMapping
-	public String create(@RequestBody ProfissionalSaude entityProfissionalSaude) {
-		ProfissionalSaude profissionalSaude = null;
-		
-		try {
-			profissionalSaude = profissionalSaudeService.save(entityProfissionalSaude);
-		} catch(Exception e) {
-			e.printStackTrace();
+	@Autowired
+	ProfissionalSaudeController(ProfissionalSaudeService profissionalSaudeService){
+		this.profissionalSaudeService = profissionalSaudeService;
+	}
+	
+	@GetMapping("/form")
+	public String form(Model model) {
+		if(model.containsAttribute("profissionalSaude")) {
+			model.addAttribute(new ProfissionalSaude());
 		}
-		return "redirect:/profissional-saude/" + profissionalSaude.getId();
+		
+		return "profissional-saude/form";
+	}
+	
+	@PostMapping("/cadastrar")
+	public String cadastrar(@Valid ProfissionalSaude profissionalSaude, BindingResult br, RedirectAttributes ra, Model model) {
+		br = profissionalSaudeService.validarProfissionalSaude(profissionalSaude, br);
+		
+		if(br.hasErrors()) {
+			model.addAttribute("message", "Erro ao cadastrar profissional da saúde");
+			model.addAttribute(profissionalSaude);
+			return form(model);
+		}
+		
+		profissionalSaudeService.cadastrarProfissional(profissionalSaude);
+		return "redirect:/profissional-saude/login" + profissionalSaude.getId();
 	}
 }
--- src/main/java/br/ufrn/PDSgrupo5/service/ProfissionalSaudeService.java
+++ src/main/java/br/ufrn/PDSgrupo5/service/ProfissionalSaudeService.java
@@ -1,41 +1,80 @@
 package br.ufrn.PDSgrupo5.service;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 
+import br.ufrn.PDSgrupo5.model.Pessoa;
 import br.ufrn.PDSgrupo5.model.ProfissionalSaude;
 import br.ufrn.PDSgrupo5.repository.ProfissionalSaudeRepository;
 
 @Service
-@Transactional (readOnly = true)
 public class ProfissionalSaudeService {
-	
 	private ProfissionalSaudeRepository profissionalSaudeRepository;
 	
+	private PessoaService pessoaService;
+	
+	private UsuarioService usuarioService;
+	
 	@Autowired
-	public ProfissionalSaudeService(ProfissionalSaudeRepository profissionalSaudeRepository) {
+	public ProfissionalSaudeService(ProfissionalSaudeRepository profissionalSaudeRepository,
+									PessoaService pessoaService, UsuarioService usuarioService) {
 		this.profissionalSaudeRepository = profissionalSaudeRepository;
+		this.pessoaService = pessoaService;
+		this.usuarioService = usuarioService;
 	}
 	
-	public List<ProfissionalSaude> findAll(){
+	public ProfissionalSaude salvar(ProfissionalSaude ps) {
+		return profissionalSaudeRepository.save(ps);
+	}
+	
+	public void cadastrarProfissional(ProfissionalSaude ps) {
+		ps.setAtivo(true);
+		ps.getPessoa().setUsuario(usuarioService.prepararUsuarioParaCriacao(ps.getPessoa().getUsuario()));
+		salvar(ps);
+	}
+	
+	public BindingResult validarProfissionalSaude(ProfissionalSaude ps, BindingResult br) {
+		if(!pessoaService.ehCpfValido(ps.getPessoa().getCpf())) {
+			br.rejectValue("pessoa.cpf", "", "CPF inválido");
+		}
+		Pessoa pessoa = pessoaService.buscarPessoaPorCpf(ps.getPessoa().getCpf());
+		if(Objects.nonNull(pessoa)) {
+			if(pessoa.getId() != ps.getPessoa().getId()) {
+				br.rejectValue("pessoa.cpf", "", "CPF já pertence a outro usuário");
+			}
+		}
+		
+		if(!ps.getPessoa().getEmail().matches("^(.+)@(.+)$")){
+            br.rejectValue("pessoa.email", "","E-mail inválido");
+        }
+		
+        if(usuarioService.loginJaExiste(ps.getPessoa().getUsuario())){
+            br.rejectValue("pessoa.usuario.login", "","Login já existe");
+        }
+        
+        pessoa = pessoaService.buscarPessoaPorEmail(ps.getPessoa().getEmail());
+        if(Objects.nonNull(pessoa)){
+            if(pessoa.getId() != ps.getPessoa().getId()){
+                br.rejectValue("pessoa.email", "","E-mail já pertence a outro usuário");
+            }
+        }
+		
+		return br;
+	}
+	
+	public List<ProfissionalSaude> listarTodosProfissionais(){
 		return profissionalSaudeRepository.findAll();
 	}
 	
-	@Transactional (readOnly = false)
-	public ProfissionalSaude save(ProfissionalSaude entity) throws Exception {
-		ProfissionalSaude aux = profissionalSaudeRepository.findByNumeroRegistro(entity.getNumeroRegistro());
-		if(aux != null) {
-			throw new Exception("Já existe cadastro com esse número de registro profissional");
-		}
-		
-		return profissionalSaudeRepository.save(entity);
+	public void excluir(ProfissionalSaude ps) {
+		profissionalSaudeRepository.delete(ps);
 	}
 	
-	@Transactional (readOnly = false)
-	public void delete(ProfissionalSaude entity) {
-		profissionalSaudeRepository.delete(entity);
+	public ProfissionalSaude buscarProfissionalPorNumeroRegistro(ProfissionalSaude ps) {
+		return profissionalSaudeRepository.findByNumeroRegistro(ps.getNumeroRegistro());
 	}
 }
