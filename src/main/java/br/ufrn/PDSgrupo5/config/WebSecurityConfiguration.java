19dffaecc395bf68619a50ad2fd20f53b9eb27ac
karinamaria <karinamaria163@gmail.com>
2021-07-13 17:15:35 -0300

    adiciona link para página de horários de atendimento. Configura rotas no webSecurity.

--- src/main/java/br/ufrn/PDSgrupo5/config/WebSecurityConfiguration.java
+++ src/main/java/br/ufrn/PDSgrupo5/config/WebSecurityConfiguration.java
@@ -27,6 +27,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/novo-paciente/salvar").permitAll()
                 .antMatchers("/novo-profissional/salvar").permitAll()
                 .antMatchers("/paciente/**").hasAuthority(EnumTipoPapel.PACIENTE.getDescricao())
+                .antMatchers("/profissional-saude/**").hasAuthority(EnumTipoPapel.PROFISSIONAL_SAUDE.getDescricao())
+                .antMatchers("/validador/**").hasAuthority(EnumTipoPapel.VALIDADOR.getDescricao())
                 .anyRequest().authenticated();
     }
     private void secureStaticResources(HttpSecurity http) throws Exception{
